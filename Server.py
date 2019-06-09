d6a1b1478ae7f7683d155a0bcb39e93079277444
lixinyang <lixinyang.albert@bytedance.com>
2019-06-10 03:24:52 +0800

    优化

--- Server.py
+++ Server.py
@@ -1,10 +1,10 @@
-from flask import Flask, render_template
+from flask import Flask
+from flask import render_template
 from flask import request
 from UserDao import UserDao
 
 
 class Server(object):
-
     app = Flask(__name__)
     hasLogin = False
 
@@ -18,7 +18,6 @@ class Server(object):
     def signin():
         # 读取表单内容并从数据库查询：
         if UserDao.searchAndReturnIfExist(request.form['username'], request.form['password']):
-
             Server.hasLogin = True
             return render_template('autojump.html', content=' 1;URL=/main', text="登录成功！1秒之后将自动登录")
         return render_template('autojump.html', content=' 2;URL=/ ', text="登录失败！2秒之后将自动跳转回登录页")
